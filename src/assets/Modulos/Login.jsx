dbe6b044262811f4fd33e2714376155462a6684a
AlanHH28 <alher2803@gmail.com>
2024-02-10 02:46:00 -0600

    Login

--- src/assets/Modulos/Login.jsx
+++ src/assets/Modulos/Login.jsx
@@ -120,7 +120,7 @@ export default function Login({ title }) {
           <p className="mt-10 text-center text-sm text-gray-500">
             ¿Aún no tienes una cuenta?{" "}
             <Link
-              href="/Registro"
+              to={"/Registro"}
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Regístrate Aquí
