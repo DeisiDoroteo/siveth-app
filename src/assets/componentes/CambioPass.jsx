eda04a4ce621c427af6e627b964cf5cb7a3821a9
Siveth <siveth.uthh03@gmail.com>
2024-02-18 18:54:40 -0600

    hide/show pass

--- src/assets/componentes/CambioPass.jsx
+++ src/assets/componentes/CambioPass.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import '../styles/Hide.css'
+
 import Label from "../componentes/ui/label.jsx";
+import Input from "./ui/input.jsx";
+import { BiHide, BiShow } from "react-icons/bi";
 
 export default function CambioPass() {
   const [ShowPwd, setShowPwd] = useState(false);
+  const [ShowPwds, setShowPwds] = useState(false);
   const [contraseniaNueva, setContraseniaNueva] = useState("");
   const [repetirContraseniaNueva, setRepetirContraseniaNueva] = useState("");
   const [error, setError] = useState("");
@@ -18,83 +23,115 @@ export default function CambioPass() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     // Validación de contraseña nueva
     if (!validarContraseniaNueva(contraseniaNueva)) {
       setError("La contraseña debe tener al menos 8 caracteres");
       return;
     }
-  
+
     // Validación de coincidencia de contraseñas
     if (contraseniaNueva !== repetirContraseniaNueva) {
       setError("Las contraseñas no coinciden");
       return;
     }
-  
+
     // Si pasa todas las validaciones, enviar la solicitud al servidor
     try {
-      console.log('Enviando solicitud con correo:', email);
-      const response = await axios.post('http://localhost:3001/cambiarContrasenia', {
-        correo: email,
-        contraseniaNueva: contraseniaNueva
-      });
-  
-      console.log('Respuesta del servidor:', response);
-  
+      console.log("Enviando solicitud con correo:", email);
+      const response = await axios.post(
+        "http://localhost:3001/cambiarContrasenia",
+        {
+          correo: email,
+          contraseniaNueva: contraseniaNueva,
+        }
+      );
+
+      console.log("Respuesta del servidor:", response);
+
       if (response.status === 200) {
-        alert('Contraseña cambiada exitosamente:');
+        alert("Contraseña cambiada exitosamente:");
         // Aquí podrías redirigir al usuario a otra página si lo deseas
-        navigate('/Login');
+        navigate("/Login");
       }
     } catch (error) {
-      console.error('Error al cambiar la contraseña:', error);
-      setError("Hubo un error al cambiar la contraseña. Por favor, inténtalo de nuevo.");
-      console.log('Correo electrónico recibido:', email);
+      console.error("Error al cambiar la contraseña:", error);
+      setError(
+        "Hubo un error al cambiar la contraseña. Por favor, inténtalo de nuevo."
+      );
+      console.log("Correo electrónico recibido:", email);
     }
   };
-  
+
   return (
-    <div >
-      <section >
-        <div div className="flex min-h-full flex-1 justify-center px-6 py-12 lg:px-8">
-          <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
-              
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight pb-2.5 text-gray-900">
-            Cambiar contraseña
+    <div>
+      <section>
+        <div
+        
+          className="flex min-h-full flex-1 justify-center px-6 py-12 lg:px-8"
+        >
+          <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md .shadow-lg sm:p-8">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight pb-2.5 text-gray-900">
+              Cambiar contraseña
             </h2>
-            <form onSubmit={handleSubmit} className="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+            <form
+              onSubmit={handleSubmit}
+              className="mt-4 space-y-4 lg:mt-5 md:space-y-5"
+            >
               <div>
-              <Label htmlFor="contraseniaNueva" >Nueva contraseña</Label>
+                <Label htmlFor="contraseniaNueva">Nueva contraseña</Label>
 
+                <div className="relative">
+                  <Input
+                    type={ShowPwd ? "text" : "password"}
+                    id="contraseniaNueva"
+                    value={contraseniaNueva}
+                    onChange={(e) => setContraseniaNueva(e.target.value)}
+                    placeholder="••••••••"
+                    required=""
+                  />
 
-
-               
-                <input
-                  type="password"
-                  id="contraseniaNueva"
-                  value={contraseniaNueva}
-                  onChange={(e) => setContraseniaNueva(e.target.value)}
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
+                  <div
+                    className={`absolute inset-y-0 top-1.5 right-0 pr-3 flex items-center text-sm leading-5 ${
+                      ShowPwd ? "" : "hide-password-icon"
+                    }`}
+                    onClick={() => setShowPwd(!ShowPwd)}
+                  >
+                    {ShowPwd ? (
+                      <BiHide className="text-indigo-600 text-xl" />
+                    ) : (
+                      <BiShow className="text-indigo-500 text-xl" />
+                    )}
+                  </div>
+                </div>
               </div>
 
-
-
-              
               <div>
-              <Label htmlFor="repetirContraseniaNueva" >Correo Electrónico</Label>
-            
-                <input
-                  type="password"
-                  id="repetirContraseniaNueva"
-                  value={repetirContraseniaNueva}
-                  onChange={(e) => setRepetirContraseniaNueva(e.target.value)}
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
+                <Label htmlFor="repetirContraseniaNueva">
+                  Repetir contraseña
+                </Label>
+                <div className="relative">
+                  <Input
+                    type={ShowPwds ? "text" : "password"}
+                    id="repetirContraseniaNueva"
+                    value={repetirContraseniaNueva}
+                    onChange={(e) => setRepetirContraseniaNueva(e.target.value)}
+                    placeholder="••••••••"
+                    required=""
+                  />
+                  <div
+                    className={`absolute inset-y-0 top-1.5 right-0 pr-3 flex items-center text-sm leading-5 ${
+                      ShowPwds ? "" : "hide-password-icon"
+                    }`}
+                    onClick={() => setShowPwds(!ShowPwds)}
+                  >
+                    {ShowPwds ? (
+                      <BiHide className="text-indigo-600 text-xl" />
+                    ) : (
+                      <BiShow className="text-indigo-500 text-xl" />
+                    )}
+                  </div>
+                </div>
               </div>
               {error && <div className="text-red-500">{error}</div>}
               <button
